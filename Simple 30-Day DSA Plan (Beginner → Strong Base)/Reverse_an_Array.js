88d98f0c90e6391d9aa807f566a8ba93ae7201fd
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2026-03-03 22:02:39 +0530

    Day 4 of learning DSA

---
+++ Simple 30-Day DSA Plan (Beginner → Strong Base)/Reverse_an_Array.js
@@ -0,0 +1,20 @@
+
+
+// this is a simple way to solve question =========
+
+// let arr = [1, 2, 3, 4, 5];
+let arr = [40, 30, 20, 10];
+
+let newarr = [];
+for (let i = arr.length - 1; i >= 0; i--) {
+    newarr.push(arr[i])
+}
+console.log(newarr);
+
+
+
+
+
+
+
+
